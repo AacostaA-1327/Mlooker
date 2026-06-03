d599939191b3ae5bfbcc4e41f29410dbec4c534a
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-06-03 18:32:34 +0100

    feat(KAN-12): endpoint regalias totales con consulta JPQL

--- api/src/main/java/com/mlooker/api/controller/InversorController.java
+++ api/src/main/java/com/mlooker/api/controller/InversorController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mlooker.api.controller.dto.InvertirRequest;
 import com.mlooker.api.controller.dto.InvertirResponse;
+import com.mlooker.api.controller.dto.TotalRegaliasResponse;
 import com.mlooker.api.entity.Inversor;
 import com.mlooker.api.service.InversorService;
 
@@ -39,6 +40,13 @@ public class InversorController {
 	public ResponseEntity<Inversor> findById(@PathVariable Long id) {
 		return inversorService.findById(id)
 				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/{id}/regalias-total")
+	public ResponseEntity<TotalRegaliasResponse> totalRegalias(@PathVariable Long id) {
+		return inversorService.totalRegaliasByInversorId(id)
+				.map(total -> ResponseEntity.ok(new TotalRegaliasResponse(id, total)))
 				.orElse(ResponseEntity.notFound().build());
 	}
 
